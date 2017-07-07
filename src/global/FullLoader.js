99305ffefaf1c60354755de222ab173387bbc008
rrsilaya <rrsilaya@up.edu.ph>
2017-07-07 23:50:33 +0800

    Fixed image loader

--- src/global/FullLoader.js
+++ src/global/FullLoader.js
@@ -5,7 +5,7 @@ import { YH_BLUE1 } from './constants';
 import { ScaleLoader } from 'halogen';
 
 const FullLoader = () =>
-  <div className="full uk-flex uk-flex-center uk-flex-middle">
+  <div className="uk-width-1-1 uk-height-1-1 uk-flex uk-flex-center uk-flex-middle">
     <div className="uk-text-center">
       <ScaleLoader color={YH_BLUE1} size="30px" />
       <br />
--- src/public-pages/global/Splash.js
+++ src/public-pages/global/Splash.js
@@ -1,35 +1,52 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import splash from '../../assets/images/splash.jpg';
+import splashOverlay from '../../assets/images/splash-overlay.png';
+import logo from '../../assets/images/yhlogo.png';
+
+import FullLoader from '../../global/FullLoader';
+
 import imagesloaded from 'imagesloaded';
 
 class Splash extends Component {
-  componentWillMount() {
+  componentDidMount() {
     imagesloaded('.img-splash', { background: '.lazy-load' }, () =>
-      console.log('done')
+      this.props.setImageLoaded()
     );
   }
 
   render() {
     return (
-      <div className="img-splash">
-        <div className="splash-title lazy-load uk-flex uk-flex-middle uk-flex-center">
-          <div className="uk-text-center">
-            <div className="splash-logo lazy-load" />
-            <h1 id="yh-tagline">YouthHack CALABARZON</h1>
-            <p className="uk-container uk-container-small white">
-              Lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum
-              dolor sit amet
-            </p>
+      <div className="img-splash uk-inline">
+        <div>
+          <div className="splash-title uk-flex uk-flex-middle uk-flex-center">
+            <div className="uk-text-center">
+              <div className="splash-logo" />
+              <h1 id="yh-tagline">YouthHack CALABARZON</h1>
+              <p className="uk-container uk-container-small white">
+                Lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem
+                ipsum dolor sit amet
+              </p>
+            </div>
+          </div>
+          <div className="splash-overlay uk-flex uk-flex-bottom uk-flex-center">
+            <Link
+              to="/about"
+              className="uk-button uk-button-large uk-margin-medium-bottom uk-text-bold learnbtn">
+              Learn More
+            </Link>
           </div>
         </div>
-        <div className="splash-overlay lazy-load uk-flex uk-flex-bottom uk-flex-center">
-          <Link
-            to="/about"
-            className="uk-button uk-button-large uk-margin-medium-bottom uk-text-bold learnbtn">
-            Learn More
-          </Link>
-        </div>
+
+        {this.props.imagesLoaded
+          ? ''
+          : [
+              <div className="uk-position-cover white-container" />,
+              <div className="uk-overlay uk-height-1-1">
+                <FullLoader />
+              </div>
+            ]}
       </div>
     );
   }
